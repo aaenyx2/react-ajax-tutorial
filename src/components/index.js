121867a6f241d8869878d52be9b978b52099bfe1
aaenyx2 <aaenyx2@mail.dgist.ac.kr>
2018-08-21 04:24:06 +0900

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@ import Navigate from './Navigate/Navigate';
 import Post from './Post/Post';
 import CommentList from './CommentList/CommentList';
 import Comment from './Comment/Comment';
+import Warning from './Warning/Warning';
 
 export {
-    Header, PostWrapper, Navigate, Post, CommentList, Comment
+    Header, PostWrapper, Navigate, Post, CommentList, Comment, Warning
 };
